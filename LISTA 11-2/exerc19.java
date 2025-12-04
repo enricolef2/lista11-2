c0b5a3ae7d19d2c8dd11193013b8bb21202fe710
enricolef2 <enricomartinscoelho@gmail.com>
2025-12-04 16:47:07 -0300

    Delete LISTA 11-2/exerc19.java

--- LISTA 11-2/exerc19.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class exerc19 {
-    public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    
-    int[][] v = new int[1][1];
-    }
-}
